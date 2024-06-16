5f21c0b00f4cf695fe6b762fe6f25195454f17b7
permana017 <ahdipermana07@gmail.com>
2024-06-16 13:52:24 +0700

    update transaction detail

--- src/component/CardGraphic/index.js
+++ src/component/CardGraphic/index.js
@@ -1,31 +1,24 @@
-import React from 'react'
-import Image from 'next/image'
-import arrow from "public/arrow-down-green.png"
-import arrowRed from "public/arrow-up-red.png"
-import graphic from "public/graphic.png"
 import { FaArrowUp } from "react-icons/fa6";
 
-
 function CardGrapich() {
-    return (
-        <>
-            <div className='flex flex-col justify-between bg-white md:shadow-xl rounded-2xl w-full md:p-5'>
-                <div className='hidden md:flex justify-between'>
-                    <div className="flex flex-col">
-                        <FaArrowUp size={28} className='text-green-500' />
-                        <h2 className="text-base">Income</h2>
-                        <h1 className='text-lg font-semibold'>Rp2.120.000</h1>
-                    </div>
-                    <div className="flex flex-col">
-                        <FaArrowUp size={28} className='text-red-500 rotate-180' />
-                        <h2 className="text-base">Expense</h2>
-                        <h1 className='text-lg font-semibold'>Rp2.120.000</h1>
-                    </div>
-                </div>
-                {/* <Image className="mt-10 md:mt-0" src={graphic} alt="" /> */}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col justify-between bg-white md:shadow-xl rounded-2xl w-full md:p-5">
+        <div className="hidden md:flex justify-between">
+          <div className="flex flex-col">
+            <FaArrowUp size={28} className="text-green-500" />
+            <h2 className="text-base">Income</h2>
+            <h1 className="text-lg font-semibold">Rp2.120.000</h1>
+          </div>
+          <div className="flex flex-col">
+            <FaArrowUp size={28} className="text-red-500 rotate-180" />
+            <h2 className="text-base">Expense</h2>
+            <h1 className="text-lg font-semibold">Rp2.120.000</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CardGrapich
+export default CardGrapich;
--- src/component/CardSaldo/index.js
+++ src/component/CardSaldo/index.js
@@ -3,40 +3,40 @@ import Topup from "../Topup";
 import Button from "src/component/Button";
 import { FaPlus } from "react-icons/fa6";
 import { FaArrowUp } from "react-icons/fa6";
-import { rupiah } from 'src/helper/formatCurrency'
+import { rupiah } from "src/helper/formatCurrency";
 
 function CardSaldo({ user, refetch, isOpen, onClose, onClick }) {
-    const router = useRouter();
-    return (
-        <>
-            <Topup isOpen={isOpen} onClose={onClose} data={user} refetch={refetch} />
-            <div className="card-compact p-0 rounded-xl w-full bg-primary text-primary-content">
-                <div className="card-body flex-row justify-between">
-                    <div className="flex flex-col justify-between">
-                        <p className="font-medium">Balance</p>
-                        <p className="text-xl md:text-2xl font-semibold">
-                            {user.saldo != null ? rupiah(user.saldo) : 0}
-                        </p>
-                        <p className="font-medium">+62 {user.phone_number ?? '-'}</p>
-                    </div>
-                    <div className="hidden md:flex flex-col justify-between gap-2">
-                        <Button onClick={() => router.push("/Transfer")} outline>
-                            <div className="flex items-center gap-2 justify-center">
-                                <FaArrowUp size={18} />
-                                TRANSFER
-                            </div>
-                        </Button>
-                        <Button onClick={onClick} outline>
-                            <div className="flex items-center gap-2 justify-center">
-                                <FaPlus size={18} />
-                                TOPUP
-                            </div>
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  const router = useRouter();
+  return (
+    <>
+      <Topup isOpen={isOpen} onClose={onClose} data={user} refetch={refetch} />
+      <div className="card-compact p-0 rounded-xl w-full bg-primary text-primary-content">
+        <div className="card-body flex-row justify-between">
+          <div className="flex flex-col justify-between">
+            <p className="font-medium">Balance</p>
+            <p className="text-xl md:text-2xl font-semibold">
+              {user?.saldo != null ? rupiah(user?.saldo) : 0}
+            </p>
+            <p className="font-medium">+62 {user.phone_number ?? "-"}</p>
+          </div>
+          <div className="hidden md:flex flex-col justify-between gap-2">
+            <Button onClick={() => router.push("/Transfer")} outline>
+              <div className="flex items-center gap-2 justify-center">
+                <FaArrowUp size={18} />
+                TRANSFER
+              </div>
+            </Button>
+            <Button onClick={onClick} outline>
+              <div className="flex items-center gap-2 justify-center">
+                <FaPlus size={18} />
+                TOPUP
+              </div>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CardSaldo;
